d50c8164622d46fc052b563da05b1091a48d8bf3
Sahil <sahil.lihas@ymail.com>
2022-02-20 20:33:46 +0530

    Update dbpedia.py

--- dbpedia.py
+++ dbpedia.py
@@ -30,25 +30,5 @@ def getData():
     # sparql = SPARQLWrapper("https://dbpedia.org/sparql", agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11")
     qres = sparql.query().convert()
     return qres
-    # pprint(qres)
-    # result = qres["results"]["bindings"][0]
-    # for result in qres["results"]["bindings"]:
-    #     text= result["name"]["value"]
-
-    # name = result["name"]["value"]
-    # description = result["description"]["value"]
-    # birth = result["birth"]["value"]
-    # description = trimDescription(description)
-    # text = "#HappyBirthday" + removeSpace(name) + "\n Birth Year:" + birth + "\n" + description
-    #
-    # return text
-    # name, birth, img_url, description = result['name']['value'], result['birth']['value'], result['img']['value'], \
-    #                                     result['description']['value']
-    # print(name)
-    # # print(img_url)
-    # # # if(len(img_url)!=0):
-    # # # urllib.request.urlretrieve(img_url, "pic.png")
-    # # print(birth)
-    # # print(description)
 
 
